7b2ead4350d533b30b7ccacaff428e1515eed9d9
Maykell Vinicius <maykellvinicius@MacBook-Pro-de-Maykell.local>
2018-05-25 00:29:12 -0300

    Criando componente que representa os detalhes do video

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB30VGwFTy3Swu3EFtqZHJVuSTG79ne_F8';
 
@@ -25,6 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={ this.state.videos[0] } />
                 <VideoList videos= { this.state.videos } />
             </div>
         );
